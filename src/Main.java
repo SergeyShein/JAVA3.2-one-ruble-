fa991bf7b81358fc9c971d75c1666f3dae77aaf1
Sergey Shein <sergeyshein20@gmail.com>
2020-12-30 10:27:14 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int current_amount = 100;
-        int replenishment_amount = 12;
-        int bonus;
-        int total_amount;
-        if (replenishment_amount <= 1000) {
-            bonus = 0;
-            total_amount = current_amount + replenishment_amount;
-            System.out.println(bonus);
-            System.out.println(total_amount);
+        int CurrentAmount = 100;
+        int ReplenishmentAmount = 12445;
+        int Bonus;
+        int TotalAmount;
+        if (ReplenishmentAmount <= 1000) {
+            Bonus = 0;
+            TotalAmount = CurrentAmount + ReplenishmentAmount;
+        } else {
+            Bonus =ReplenishmentAmount / 100;
+            TotalAmount = CurrentAmount + ReplenishmentAmount + Bonus;
         }
-        if (replenishment_amount > 1000) {
-            bonus = replenishment_amount / 100;
-            total_amount = current_amount + replenishment_amount + bonus;
-            System.out.println(bonus);
-            System.out.println(total_amount);
-        }
+        System.out.println(Bonus);
+        System.out.println(TotalAmount);
     }
 }
